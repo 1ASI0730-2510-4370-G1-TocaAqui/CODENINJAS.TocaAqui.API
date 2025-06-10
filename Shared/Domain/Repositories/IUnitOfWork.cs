df4cde8726e232e47953d49ba497d4ebc0cc2c56
Oscar Antayhua Castillo <u20191e414@upc.edu.pe>
2025-06-09 22:12:59 -0500

    add shared domain repositories

---
+++ Shared/Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+namespace tocaaqui_backend.Shared.Domain.Repositories;
+
+/// <summary>
+///     Unit of Work interface for managing database transactions
+/// </summary>
+public interface IUnitOfWork
+{
+    Task<int> CompleteAsync();
+} 
